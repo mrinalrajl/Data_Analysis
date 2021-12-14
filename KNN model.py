e12c480b408341b54030b8d5fbbdd4e0699341fd
Lakkimsetty Mrinal Raj <60313820+Mrinal12324@users.noreply.github.com>
2021-12-14 18:59:41 +0530

    first commit

---
+++ KNN model.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[3]:
+
+
+df=pd.read_csv("Downloads/ML/knn.csv")
+
+
+# In[4]:
+
+
+df
+
+
+# ## KNN (K-Nearest Neighbour)
+
+# 
